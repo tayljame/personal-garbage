217ab46947014b60d1ed5d3bf03eb2a1e7cbf31b
tayljame <tayljame@oregonstate.edu>
2015-10-08 18:51:12 -0600

    first test 6:45 10/8

---
+++ newsroomquotes.c
@@ -0,0 +1,5 @@
+First of all, I'm a registered republican. 
+I only seem liberal because I believe hurricanes are caused by high barometric pressure
+and not gay marriage
+
+I've decided to live my life as if I'm alive
